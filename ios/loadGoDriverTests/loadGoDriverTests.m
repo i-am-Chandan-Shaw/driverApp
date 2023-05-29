292587e4c23babf966af2b81e0dbb82737994ab4
i-am-Chandan-Shaw <chandan.shaw2050@gmail.com>
2023-05-29 17:54:49 +0530

    app renamed

--- ios/loadGoDriverTests/loadGoDriverTests.m
+++ ios/loadGoDriverTests/loadGoDriverTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface loadGoDriverTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation loadGoDriverTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
