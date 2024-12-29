0f29c588695a07371807c1ee3eaa7b8ab242f508
Chandan Shaw <chandan.shaw2050@gmail.com>
2024-12-29 17:06:47 +0530

    fixed notification request access

--- App.tsx
+++ App.tsx
@@ -3,25 +3,30 @@ import { AppProvider } from "./src/core/helper/AppContext";
 import { ThemeProvider } from "./src/constants/ThemeContext";
 import Navigation from "./src/navigation";
 import messaging from "@react-native-firebase/messaging";
-import { checkAppVersion } from "./src/core/helper/apiHelper";
+import {
+  checkAppVersion,
+  requestNotificationPermission,
+} from "./src/core/helper/apiHelper";
 
 function App() {
   useEffect(() => {
-    const requestPermission = async () => {
-      const authStatus = await messaging().requestPermission();
-      const isAuthorized =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-      if (isAuthorized) {
-        console.log("User granted messaging permissions");
-      } else {
-        console.warn("Messaging permissions not granted");
-      }
-    };
     checkAppVersion();
     requestPermission();
+    requestNotificationPermission();
   }, []);
+
+  const requestPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const isAuthorized =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (isAuthorized) {
+      console.log("User granted messaging permissions");
+    } else {
+      console.warn("Messaging permissions not granted");
+    }
+  };
   return (
     <ThemeProvider>
       <AppProvider>
--- src/core/helper/apiHelper.js
+++ src/core/helper/apiHelper.js
@@ -1,7 +1,7 @@
 import DeviceInfo from "react-native-device-info";
 import { patch } from "./services";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Alert } from "react-native";
+import { Alert, Linking, PermissionsAndroid, Platform } from "react-native";
 
 export const updateDriverPushToken = async (driverId, pushToken) => {
   const payload = {
@@ -45,5 +45,44 @@ const clearCache = async () => {
     console.log("Cache cleared successfully.");
   } catch (error) {
     console.error("Failed to clear cache:", error);
+  }
+};
+
+export const requestNotificationPermission = async () => {
+  if (Platform.OS === "android" && Platform.Version >= 33) {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        {
+          title: "Notification Permission",
+          message: "Enable notifications to stay updated with nearby rides.",
+
+          buttonPositive: "OK",
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Notification permission granted");
+      } else {
+        console.log("Notification permission denied");
+        Alert.alert(
+          "Permission Needed",
+          "You have permanently denied notification access. Please enable it manually in the app settings.",
+          [
+            { text: "Cancel", style: "cancel" },
+            {
+              text: "Open Settings",
+              onPress: () => Linking.openSettings(),
+            },
+          ]
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  } else {
+    console.log(
+      "Notification permission not required for this Android version."
+    );
   }
 };
